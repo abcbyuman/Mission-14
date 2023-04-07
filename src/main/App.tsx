9c3806816ce62a6e6a947a06eb1526f6ba10f800
abcbyuman <95644079+abcbyuman@users.noreply.github.com>
2023-04-07 14:57:06 -0600

    Mission 14 updates

---
+++ src/main/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../Header";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Header slogan="Man, you must be sick of hearing my voice by now! - Home" />
+    </div>
+  );
+}
+
+export default App;
